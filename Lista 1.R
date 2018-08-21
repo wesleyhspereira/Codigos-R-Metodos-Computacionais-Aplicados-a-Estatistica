023bfa6fcf81adba9d86dd4170d12697abb0430e
wesleyhspereira <39173544+wesleyhspereira@users.noreply.github.com>
2018-08-21 20:37:31 -0300

    Add files via upload

---
+++ Lista 1.R
@@ -0,0 +1,75 @@
+########### Aula 1 - Ex 4 ##########
+
+fross4.2 = function(q)
+{
+  U = runif(1)
+  i = 0
+  p = exp(-q)
+  FP = p
+  while(U >= FP)
+  {
+    p = (q*p)/(i+1)
+    FP = FP + p
+    i = i + 1
+  }
+  return(i)
+}
+
+rpoisson = function(n,q)
+{
+  X = rep(q,n)
+  return(sapply(X,fross4.2))
+}
+
+########### Aula 1 - Ex 5 ##########
+
+fross4h.1 = function(v)
+{
+  N = length(v) - 1
+  i = as.numeric(names(sort(v[match(names(v[v < 1/N]),names(v))])[1]))
+  z = v[i] + v[-i]
+  j = as.numeric(names(sort(v[match(names(z[z >= 1/N]),names(v))]))[1])
+  k = as.numeric(match(i,as.numeric(names(v))))
+  q = N*v[k]
+  q[2] = 1 - q
+  names(q) = c(i,j)
+  return(q)
+}
+
+fross4h.2 = function(p)
+{
+  q = list()
+  N = length(p)
+  t = N - 1
+  for(j in 1:(N-2))
+  {
+    q[[j]] = fross4h.1(p)
+    i = match(names(q[[j]]),names(p))
+    c = (1/t)*q[[j]]
+    p[i[1]] = p[i[1]] - c[1]
+    p[i[2]] = p[i[2]] - c[2]
+    p = p[-which(p == 0)]*(t/(t-1))
+    t = t - 1
+  }
+  q[[j+1]] = p
+  return(q)
+}
+
+fross4h.3 = function(q,p)
+{
+  n = length(p)
+  N = ceiling((n-1)*runif(1))
+  u = runif(1)
+  g = as.numeric((names(q[[N]])))
+  x = ifelse(u<as.numeric(q[[N]][1]),g[1],g[2])
+  return(x)
+}
+
+rmultimix = function(n,p)
+{
+  names(p) = 1:length(p)
+  q = fross4h.2(p)
+  x = replicate(n,fross4h.3(q,p))
+  return(x)
+}
+
