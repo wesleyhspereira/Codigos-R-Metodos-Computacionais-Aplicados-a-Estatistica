30f6e21d1236a7544e515928cf27d369b2784006
pedroaraujo9 <pedromenezes98@gmail.com>
2018-08-22 15:32:26 -0300

    incluindo ex3
    teste

--- Lista 1.R
+++ Lista 1.R
@@ -1,3 +1,52 @@
+########### Aula 1 - Ex 3 ##########
+#3.11
+set.seed(10)
+################
+rmixp = function(n, p) {
+  *rnorm(n) + *rnorm(n, 3, 1)
+}
+
+dmixp = function(x, p) {
+  p*dnorm(x) + (1-p)*dnorm(x, 3, 1)
+}
+
+
+pl = function(p, n=1000) {
+  amostra = mixp(n, p)
+  x = seq(min(amostra)-1, max(amostra)+1, 0.001)
+  hist(amostra, freq = F, main="Histrograma da amostra simulada")
+  lines(x, dmixp(x, p), col='red')
+  
+}
+
+
+pl(p=0.75)
+pl(p=0.6)
+pl(p=0.5)
+
+#p=0.75
+amostra = mixp(1000, 0.75)
+x = seq(min(amostra), max(amostra), 0.001)
+hist(amostra, freq = F)
+lines(x, dmixp(x, 0.75))
+
+#p=0.75
+amostra = mixp(1000, 0.75)
+x = seq(min(amostra), max(amostra), 0.001)
+hist(amostra, freq = F)
+lines(x, dmixp(x, 0.75))
+
+#p=0.75
+amostra = mixp(1000, 0.75)
+x = seq(min(amostra), max(amostra), 0.001)
+hist(amostra, freq = F)
+lines(x, dmixp(x, 0.75))
+
+
+hist(mixp(1000, 0.75))
+lines(x, f(x))
+
+
 ########### Aula 1 - Ex 4 ##########
 
 fross4.2 = function(q)
